081bf1c00439c8a9ed5898c99d4645348356c4dd
jonathn6 <jonathn6@icloud.com>
2021-11-20 16:05:41 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+
+namespace KanyeWest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("KanyeWest!");
+
+            var clientKanye = new HttpClient();
+            var clientRon = new HttpClient();
+            var kanyeURL = "https://api.kanye.rest";
+            var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+            var kanyeResponse = "";
+            var ronResponse = "";
+
+            var kanyeQuote = "";
+            var ronQuote = "";
+
+            int loopCounter = 0;
+
+            do
+            {
+                kanyeResponse = clientKanye.GetStringAsync(kanyeURL).Result;
+                ronResponse = clientRon.GetStringAsync(ronURL).Result;
+
+                kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+                ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+                Console.WriteLine($"Kanye: {kanyeQuote}");
+                Console.WriteLine($"Ron: {ronQuote}");
+                Console.WriteLine("");
+                loopCounter++;
+
+            } while (loopCounter < 5);
+
+        }
+    }
+}
